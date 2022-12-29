308cf453d7245d93d31f952bc372dc07df4fab53
srish0809 <srashtisoni2001.ss@gmail.com>
2022-12-29 16:29:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+
+const app=express();
+
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(shopRoutes);
+app.use(adminRoutes);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>');
+});
+
+app.listen(3000);
